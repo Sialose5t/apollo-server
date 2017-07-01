Sialose5t/apollo-server

commit 82b6a503f36667fb87c0ae241a280e0d6effbb3d

change how koa error response is returned

packages/graphql-server-koa/src/koaApollo.ts
@@ -54,6 +54,9 @@ export function graphiqlKoa(options: GraphiQL.GraphiQLData | KoaGraphiQLOptionsF
     GraphiQL.resolveGraphiQLString(query, options, ctx).then(graphiqlString => {
       ctx.set('Content-Type', 'text/html');
       ctx.body = graphiqlString;
-    }, error => ctx.throw(error.message, 500));
+    }, error => {
+      ctx.status = 500;
+      ctx.body = error.message;
+    });
   };
 }

commit 48310fc178acbd0075a8e2f5abb393a8488728c2

Fixes broken graphiql endpoint
It looks like 82b6a50 broke the graphiql endpoint by no longer returning the promise.

packages/graphql-server-koa/src/koaApollo.ts
@@ -51,7 +51,7 @@ export interface KoaGraphiQLOptionsFunction {
 export function graphiqlKoa(options: GraphiQL.GraphiQLData | KoaGraphiQLOptionsFunction) {
   return (ctx: koa.Context) => {
     const query = ctx.request.query;
-    GraphiQL.resolveGraphiQLString(query, options, ctx).then(graphiqlString => {
+    return GraphiQL.resolveGraphiQLString(query, options, ctx).then(graphiqlString => {
       ctx.set('Content-Type', 'text/html');
       ctx.body = graphiqlString;
     }, error => {